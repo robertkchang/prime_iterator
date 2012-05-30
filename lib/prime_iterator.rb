113ee5e7aa81803d9d21c129892e31276f3510eb
Robert Chang <rchang@fulcrum.net>
2012-05-30 15:37:49 -0700

    Cleaned up code and tests

--- lib/prime_iterator.rb
+++ lib/prime_iterator.rb
@@ -8,34 +8,34 @@ class PrimeIterator
   BATCH_SIZE = 10
 
   def initialize
-    @current_lower_bound = 2  # init to 2
-    @current_upper_bound = 2  # init to 2
-    @current_prime_batch = Array.new
-    @current_prime_batch_idx = 0
+    @current_lower_bound = 2  # 2 is first prime number
+    @current_upper_bound = 2  #
+    @current_prime_list = Array.new
+    @current_prime_list_idx = -1
   end
 
   def next
-    if @current_prime_batch.empty? || @current_prime_batch_idx == BATCH_SIZE-1
+    # if list is empty or index is pointing to end of list, get next batch of prime numbers
+    if @current_prime_list.empty? || @current_prime_list_idx == BATCH_SIZE-1
       get_next_batch_of_primes
     end
 
-    @current_prime_batch_idx += 1
-    @current_prime_batch[@current_prime_batch_idx]
+    @current_prime_list_idx += 1
+    @current_prime_list[@current_prime_list_idx]
   end
 
   def previous
-    if @current_prime_batch.empty?
-      return nil
-    end
+    return nil if @current_prime_list.empty?
 
-    if @current_prime_batch_idx == 0
+    # if at top of list, then get previous batch of prime numbers
+    if @current_prime_list_idx == 0
       get_prev_batch_of_primes
-      @current_prime_batch_idx = BATCH_SIZE - 1
+      @current_prime_list_idx = BATCH_SIZE - 1 # 0 based index
     else
-      @current_prime_batch_idx -= 1
+      @current_prime_list_idx -= 1
     end
 
-    @current_prime_batch[@current_prime_batch_idx]
+    @current_prime_list[@current_prime_list_idx] unless @current_prime_list.empty? # could be empty if previous is called on prime number 2
   end
 
   def reset
@@ -45,33 +45,33 @@ class PrimeIterator
   private #####
 
   def get_next_batch_of_primes
-    @current_prime_batch.clear
+    @current_prime_list.clear
 
     # keep getting prime numbers until batch size is reached
     begin
       finder = PrimeFinder.new
-      @current_prime_batch = finder.find_primes @current_lower_bound, @current_upper_bound
+      @current_prime_list = finder.find_primes @current_lower_bound, @current_upper_bound
       @current_upper_bound += 1
-    end until @current_prime_batch.size == BATCH_SIZE
+    end until @current_prime_list.size == BATCH_SIZE
 
     @current_lower_bound = @current_upper_bound
-    @current_prime_batch_idx = -1
+    @current_prime_list_idx = -1 # set to before top of list
   end
 
   def get_prev_batch_of_primes
-    @current_upper_bound = @current_prime_batch[0] - 1 # minus 1 so it doesn't include the current prime number
-    @current_prime_batch.clear
+    @current_upper_bound = @current_prime_list[0] - 1 # minus 1 so it doesn't include the current prime number
+    @current_prime_list.clear
 
     finder = PrimeFinder.new
     previous_prime_batch = finder.find_primes @current_upper_bound
-    @current_prime_batch = previous_prime_batch.last(BATCH_SIZE)  # truncate the list to the last BATCH_SIZE number of prime numbers
+    @current_prime_list = previous_prime_batch.last(BATCH_SIZE)  # truncate the list to the last BATCH_SIZE number of prime numbers
 
-    if @current_prime_batch.empty?
+    if @current_prime_list.empty?
       # keep previous lower bound
       # set upper bound equal to lower bound
       @current_upper_bound = @current_lower_bound
     else
-      # otherwise, set the lower bound to be the first prime numbner in this batch
+      # otherwise, set the lower bound to be the first prime number in this batch
       @current_lower_bound = previous_prime_batch.first
     end
   end
--- spec/prime_iterator_spec.rb
+++ spec/prime_iterator_spec.rb
@@ -4,348 +4,159 @@ require 'prime_iterator'
 describe "Test" do
   it "should get first four prime numbers starting with 2" do
     iterator = PrimeIterator.new
-
-    next_prime = iterator.next
-    next_prime.should eql 2
-
-    next_prime = iterator.next
-    next_prime.should eql 3
-
-    next_prime = iterator.next
-    next_prime.should eql 5
-
-    next_prime = iterator.next
-    next_prime.should eql 7
+    iterator.next.should eql 2
+    iterator.next.should eql 3
+    iterator.next.should eql 5
+    iterator.next.should eql 7
   end
 
   it "should get first ten prime numbers starting with 2" do
     iterator = PrimeIterator.new
-
-    next_prime = iterator.next
-    next_prime.should eql 2
-
-    next_prime = iterator.next
-    next_prime.should eql 3
-
-    next_prime = iterator.next
-    next_prime.should eql 5
-
-    next_prime = iterator.next
-    next_prime.should eql 7
-
-    next_prime = iterator.next
-    next_prime.should eql 11
-
-    next_prime = iterator.next
-    next_prime.should eql 13
-
-    next_prime = iterator.next
-    next_prime.should eql 17
-
-    next_prime = iterator.next
-    next_prime.should eql 19
-
-    next_prime = iterator.next
-    next_prime.should eql 23
-
-    next_prime = iterator.next
-    next_prime.should eql 29
+    iterator.next.should eql 2
+    iterator.next.should eql 3
+    iterator.next.should eql 5
+    iterator.next.should eql 7
+    iterator.next.should eql 11
+    iterator.next.should eql 13
+    iterator.next.should eql 17
+    iterator.next.should eql 19
+    iterator.next.should eql 23
+    iterator.next.should eql 29
   end
 
   it "should get eleven prime numbers starting with 2" do
     iterator = PrimeIterator.new
-
-    next_prime = iterator.next
-    next_prime.should eql 2
-
-    next_prime = iterator.next
-    next_prime.should eql 3
-
-    next_prime = iterator.next
-    next_prime.should eql 5
-
-    next_prime = iterator.next
-    next_prime.should eql 7
-
-    next_prime = iterator.next
-    next_prime.should eql 11
-
-    next_prime = iterator.next
-    next_prime.should eql 13
-
-    next_prime = iterator.next
-    next_prime.should eql 17
-
-    next_prime = iterator.next
-    next_prime.should eql 19
-
-    next_prime = iterator.next
-    next_prime.should eql 23
-
-    next_prime = iterator.next
-    next_prime.should eql 29
+    iterator.next.should eql 2
+    iterator.next.should eql 3
+    iterator.next.should eql 5
+    iterator.next.should eql 7
+    iterator.next.should eql 11
+    iterator.next.should eql 13
+    iterator.next.should eql 17
+    iterator.next.should eql 19
+    iterator.next.should eql 23
+    iterator.next.should eql 29
 
     # should get next batch of prime numbers here
-
-    next_prime = iterator.next
-    next_prime.should eql 31
+    iterator.next.should eql 31
   end
 
   it "should reset and restart prime number sequence from 2" do
     iterator = PrimeIterator.new
-
-    next_prime = iterator.next
-    next_prime.should eql 2
-
-    next_prime = iterator.next
-    next_prime.should eql 3
-
-    next_prime = iterator.next
-    next_prime.should eql 5
-
-    next_prime = iterator.next
-    next_prime.should eql 7
+    iterator.next.should eql 2
+    iterator.next.should eql 3
+    iterator.next.should eql 5
+    iterator.next.should eql 7
 
     iterator.reset
-
-    next_prime = iterator.next
-    next_prime.should eql 2
+    iterator.next.should eql 2
   end
 
   it "should get previous four prime numbers" do
     iterator = PrimeIterator.new
-
-    next_prime = iterator.next
-    next_prime.should eql 2
-
-    next_prime = iterator.next
-    next_prime.should eql 3
-
-    next_prime = iterator.next
-    next_prime.should eql 5
-
-    next_prime = iterator.next
-    next_prime.should eql 7
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 5
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 3
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 2
+    iterator.next.should eql 2
+    iterator.next.should eql 3
+    iterator.next.should eql 5
+    iterator.next.should eql 7
+    iterator.previous.should eql 5
+    iterator.previous.should eql 3
+    iterator.previous.should eql 2
 
     # previous at 2 will always return nil
-
-    prev_prime = iterator.previous
-    prev_prime.should eql nil
+    iterator.previous.should eql nil
   end
 
   it "should get previous ten prime numbers when previous is called on eleventh prime" do
     iterator = PrimeIterator.new
-
-    next_prime = iterator.next
-    next_prime.should eql 2
-
-    next_prime = iterator.next
-    next_prime.should eql 3
-
-    next_prime = iterator.next
-    next_prime.should eql 5
-
-    next_prime = iterator.next
-    next_prime.should eql 7
-
-    next_prime = iterator.next
-    next_prime.should eql 11
-
-    next_prime = iterator.next
-    next_prime.should eql 13
-
-    next_prime = iterator.next
-    next_prime.should eql 17
-
-    next_prime = iterator.next
-    next_prime.should eql 19
-
-    next_prime = iterator.next
-    next_prime.should eql 23
-
-    next_prime = iterator.next
-    next_prime.should eql 29
+    iterator.next.should eql 2
+    iterator.next.should eql 3
+    iterator.next.should eql 5
+    iterator.next.should eql 7
+    iterator.next.should eql 11
+    iterator.next.should eql 13
+    iterator.next.should eql 17
+    iterator.next.should eql 19
+    iterator.next.should eql 23
+    iterator.next.should eql 29
 
     # should get next batch of prime numbers here
-
-    next_prime = iterator.next
-    next_prime.should eql 31
+    iterator.next.should eql 31
 
     # should get previous batch here
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 29
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 23
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 19
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 17
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 13
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 11
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 7
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 5
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 3
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 2
-
-    prev_prime = iterator.previous
-    prev_prime.should eql nil
+    iterator.previous.should eql 29
+    iterator.previous.should eql 23
+    iterator.previous.should eql 19
+    iterator.previous.should eql 17
+    iterator.previous.should eql 13
+    iterator.previous.should eql 11
+    iterator.previous.should eql 7
+    iterator.previous.should eql 5
+    iterator.previous.should eql 3
+    iterator.previous.should eql 2
+    iterator.previous.should eql nil
   end
 
   it "should get next 10 then previous 10 then next 10 again" do
     iterator = PrimeIterator.new
-
-    next_prime = iterator.next
-    next_prime.should eql 2
-
-    next_prime = iterator.next
-    next_prime.should eql 3
-
-    next_prime = iterator.next
-    next_prime.should eql 5
-
-    next_prime = iterator.next
-    next_prime.should eql 7
-
-    next_prime = iterator.next
-    next_prime.should eql 11
-
-    next_prime = iterator.next
-    next_prime.should eql 13
-
-    next_prime = iterator.next
-    next_prime.should eql 17
-
-    next_prime = iterator.next
-    next_prime.should eql 19
-
-    next_prime = iterator.next
-    next_prime.should eql 23
-
-    next_prime = iterator.next
-    next_prime.should eql 29
+    iterator.next.should eql 2
+    iterator.next.should eql 3
+    iterator.next.should eql 5
+    iterator.next.should eql 7
+    iterator.next.should eql 11
+    iterator.next.should eql 13
+    iterator.next.should eql 17
+    iterator.next.should eql 19
+    iterator.next.should eql 23
+    iterator.next.should eql 29
 
     # should get next batch of prime numbers here
-
-    next_prime = iterator.next
-    next_prime.should eql 31
+    iterator.next.should eql 31
 
     # should get previous batch here
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 29
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 23
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 19
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 17
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 13
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 11
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 7
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 5
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 3
-
-    prev_prime = iterator.previous
-    prev_prime.should eql 2
-
-    prev_prime = iterator.previous
-    prev_prime.should eql nil
+    iterator.previous.should eql 29
+    iterator.previous.should eql 23
+    iterator.previous.should eql 19
+    iterator.previous.should eql 17
+    iterator.previous.should eql 13
+    iterator.previous.should eql 11
+    iterator.previous.should eql 7
+    iterator.previous.should eql 5
+    iterator.previous.should eql 3
+    iterator.previous.should eql 2
+    iterator.previous.should eql nil
 
     # should get next batch here
-
-    next_prime = iterator.next
-    next_prime.should eql 2
-
-    next_prime = iterator.next
-    next_prime.should eql 3
-
-    next_prime = iterator.next
-    next_prime.should eql 5
-
-    next_prime = iterator.next
-    next_prime.should eql 7
-
-    next_prime = iterator.next
-    next_prime.should eql 11
-
-    next_prime = iterator.next
-    next_prime.should eql 13
-
-    next_prime = iterator.next
-    next_prime.should eql 17
-
-    next_prime = iterator.next
-    next_prime.should eql 19
-
-    next_prime = iterator.next
-    next_prime.should eql 23
-
-    next_prime = iterator.next
-    next_prime.should eql 29
+    iterator.next.should eql 2
+    iterator.next.should eql 3
+    iterator.next.should eql 5
+    iterator.next.should eql 7
+    iterator.next.should eql 11
+    iterator.next.should eql 13
+    iterator.next.should eql 17
+    iterator.next.should eql 19
+    iterator.next.should eql 23
+    iterator.next.should eql 29
 
     # should get next batch of prime numbers here
-
-    next_prime = iterator.next
-    next_prime.should eql 31
+    iterator.next.should eql 31
   end
 
   it "should get prime numbers unique to the instance" do
     iterator = PrimeIterator.new
     iterator2 = PrimeIterator.new
 
-    next_prime = iterator.next
-    next_prime.should eql 2
-
-    next_prime = iterator.next
-    next_prime.should eql 3
-
-    next_prime = iterator.next
-    next_prime.should eql 5
-
-    next_prime = iterator.next
-    next_prime.should eql 7
+    iterator.next.should eql 2
+    iterator.next.should eql 3
+    iterator.next.should eql 5
+    iterator.next.should eql 7
 
     # second instance should return 2
-    next_prime2 = iterator2.next
-    next_prime2.should eql 2
+    iterator2.next.should eql 2
+  end
+
+  it "should return nil if previous is called on newly instantiated iterator" do
+    iterator = PrimeIterator.new
+    iterator.previous.should eql nil
   end
 
 end
